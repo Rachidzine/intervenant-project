bb923bec9b80133e1f239c71a61902a3dd39fba8
razi <rachidzine@sfr.fr>
2020-09-27 12:45:46 +0200

    add showFormForAdd  method in controller

--- src/main/java/com/stitec/Myproject/controller/IntervenantController.java
+++ src/main/java/com/stitec/Myproject/controller/IntervenantController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.stitec.Myproject.entity.Intervenant;
@@ -29,4 +30,16 @@ public class IntervenantController {
 		return "intervenants/list-intervenants";
 	}
 
+	
+	//-----------------------------------------------------------------------------------
+	@GetMapping("/showForForAdd")
+	public String  showFormForAdd(Model theModel) {
+		
+		//creation du l'attribute model pour la liason de donnee
+		Intervenant theIntervenant = new Intervenant();
+		
+		theModel.addAttribute("intervenant", theIntervenant);
+		
+		return "intervenants/intervenant-form";
+	}
 }
