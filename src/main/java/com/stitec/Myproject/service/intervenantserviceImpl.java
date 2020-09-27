f300948db001c8c0987f5ff7d5cc68b5923b1051
razi <rachidzine@sfr.fr>
2020-09-27 13:49:43 +0200

    modif service for custom method findAllByOrderByLastNameAsc()

--- src/main/java/com/stitec/Myproject/service/intervenantserviceImpl.java
+++ src/main/java/com/stitec/Myproject/service/intervenantserviceImpl.java
@@ -24,7 +24,7 @@ public class intervenantserviceImpl implements IntervenantService {
 	@Override
 	public List<Intervenant> findAll() {
 		
-		return intervenantRepository.findAll();
+		return intervenantRepository.findAllByOrderByLastNameAsc();
 	}
 
 	//---------------SAVE---------------------------------
