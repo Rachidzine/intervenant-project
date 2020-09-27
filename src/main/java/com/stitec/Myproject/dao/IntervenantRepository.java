d23d39c99dc5b30e5fa59fc03a8af010e8f17667
razi <rachidzine@sfr.fr>
2020-09-27 11:33:46 +0200

    dao interface created

---
+++ src/main/java/com/stitec/Myproject/dao/IntervenantRepository.java
@@ -0,0 +1,10 @@
+package com.stitec.Myproject.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.stitec.Myproject.entity.Intervenant;
+
+
+public interface IntervenantRepository extends JpaRepository<Intervenant, Integer> {
+
+}
